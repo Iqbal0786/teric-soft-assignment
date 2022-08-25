ad44b2393dd7ccde429e8228f5718a6649e67d40
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-25 21:20:04 +0530

    added components

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../styles/style.css"
+
+export default function Home() {
+  return (
+    <>
+     <div id='slideDiv'>
+
+     </div>
+     <div id="header_Menu">
+        <ul>
+            <li>BMS</li>
+            <li>Mountains</li>
+            <li>Road</li>
+            <li>Tracks</li>
+        </ul>
+     </div>
+      <div id='borderDiv'></div>
+
+      <div id='HighlightDiv'>
+         <div id='highlightTitle'>Highlights</div>
+      </div>
+    </>
+  )
+}
